59c950ba2a92638b24499a57a5bd6965a8f079d5
paula.guatura <paula.guatura@dextra-sw.com>
2018-07-04 10:19:35 -0300

    fix

--- issues.sh
+++ issues.sh
@@ -28,7 +28,7 @@ fi
 
 if [[ "$projectId" == "" ]];then
     projects=$($dir/projects.sh -t $token | jq '.')
-    text=$(echo $projects | jq -r '. | sort_by(.name) | to_entries[] | " \(.key)) (\(.value.platform)) \(.value.name) - \(.value.identifier)"')
+    text=$(echo $projects | jq -r '. | sort_by(.name) | to_entries[] | " \(.key)) (\(.value.platform)) \(.value.name) - \(.value.identifier) (\(.value.externalId))"')
     text="$text"
     echo -e "Choose project:\n\n$text\n\nProject number, followed by [ENTER]:"
     read projectNumber
@@ -66,14 +66,24 @@ if [ "$versionId" == "Z" ];then
     versionId=$(echo $versions | jq '[. | sort_by(-.sortOrder)[0:4][] | .externalId] ')
 fi
 versionName=$(echo $versions | jq --arg id "[\"$versionId\"]" '[.[] | select(.externalId | inside($id) ) | .name]')
-versionName=$(echo $versionName | sed 's/"/\\\\\"/g')
+versionName=$(echo $versionName | sed 's/"/\\"/g')
 
 startDate=$(date -u -d "00:00:00 $numDays days ago" +%s)
 endDate=$(date -u -d "23:59:59" +%s)
 externalId=$projectId
 
-query=$(echo '{"query":"query TopIssues($externalId_0:String!,$type_1:IssueType!,$start_2:UnixTimestamp!,$end_3:UnixTimestamp!,$filters_4:IssueFiltersType!,$state_5:IssueState!) {project(externalId:$externalId_0) {crashlytics {_issues1ctixL:issues(synthesizedBuildVersions:{versions},eventType:$type_1,start:$start_2,end:$end_3,state:$state_5,first:15,filters:$filters_4) {edges {node {externalId,createdAt,resolvedAt,title,subtitle,state,type,occurrenceCount,earliestBuildVersion {buildVersion {name}},latestBuildVersion {buildVersion {name}}}}}}}}","variables":{"externalId_0":"{externalId}","type_1":"{type}","start_2":{start},"end_3":{end},"filters_4":{},"state_5":"{status}"}}' | sed "s/{status}/$status/g" | sed "s/{end}/$endDate/g" | sed "s/{start}/$startDate/g" | sed "s/{versions}/$versionName/g" | sed "s/{externalId}/$externalId/g" | sed "s/{type}/$type/g")
-
+query="{\"query\":\"query TopIssues( \$externalId_0:String! , \$type_1:IssueType! , \
+    \$start_2:UnixTimestamp! , \$end_3:UnixTimestamp! , \$filters_4:IssueFiltersType! , \
+    \$state_5:IssueState! ) \
+    {project(externalId: \$externalId_0 ) {crashlytics { \
+        _issues1ctixL:issues(synthesizedBuildVersions:$versionName,eventType: \$type_1, \
+        start:\$start_2,end:\$end_3,state:\$state_5,first:15,filters:\$filters_4) \
+        {edges {node {externalId,createdAt,resolvedAt,title,subtitle,state,type, \
+        occurrenceCount,earliestBuildVersion {buildVersion {name}}, \
+        latestBuildVersion {buildVersion {name}}}}}}}}\",\"variables\": \
+        {\"externalId_0\":\"$externalId\",\"type_1\":\"$type\", \
+        \"start_2\":$startDate,\"end_3\":$endDate, \
+        \"filters_4\":{},\"state_5\":\"$status\"}}"
 
 data=$(curl -s 'https://api-dash.fabric.io/graphql?relayDebugName=TopIssues' \
 -H "Authorization: Bearer $token" \
